addc138b03b795db0fc46f2bb8c67eb585367275
Karin Wegner <karinwegner00@gmail.com>
2025-03-25 12:22:06 +0100

    Finished csv document generation

--- powergrid_data_gen/DocumentWriter.cs
+++ powergrid_data_gen/DocumentWriter.cs
@@ -43,7 +43,7 @@ namespace powergrid_data_gen
             // Write the JSON string to a file
              File.WriteAllText(filePath, jsonData);
 
-            string csvfilePath = "C:\\Users\\karin\\source\\repos\\powergrid_data_gen\\powergrid_data_gen\\documents\\log_output.csv";
+            string csvfilePath = "C:\\Users\\karin\\source\\repos\\powergrid_data_gen\\powergrid_data_gen\\documents\\";
             CsvWriter.WriteLogObjectToCsv(Data, csvfilePath);
 
             Console.WriteLine("JSON file created at: " + filePath);
@@ -60,25 +60,46 @@ public class CsvWriter
                 throw new ArgumentException("Log list is empty or null."); 
 
 
-            var sb = new StringBuilder();
-
+            int logCount = 0;
             // CSV Header
             foreach (var logObject in logList)
             {
-                if (logObject.component?.loggedData == null)
-                    continue; // Skip if there's no log data
-
-                foreach (var log in logObject.component.loggedData)
+            var sb = new StringBuilder();
+                if (logObject is TransformerLogObject)
                 {
-                    sb.AppendLine($"{logObject.LogStart:yyyy-MM-dd HH:mm:ss},{logObject.LogEnd:yyyy-MM-dd HH:mm:ss}," +
-                                  $"{logObject.component.spec?.component_id}, {logObject.component.spec?.length_km},{logObject.component.spec?.current_capacity},{logObject.component.spec?.max_operating_temperature}," +
-                                  $"{logObject.component.spec?.conductor_material},{logObject.component.spec?.line_type},{logObject.component.spec?.insulation_type}," +
-                                  $"{log.timestamp:yyyy-MM-dd HH:mm:ss},{log.power_capacity},{log.line_voltage}");
+                    TransformerLogObject transobj = logObject as TransformerLogObject;
+                    if (transobj.component?.loggedData == null)
+                    continue; // Skip if there's no log data
+                    sb.AppendLine("Log start, Log end, Component Id, Component type, Power rating, Primary voltage, Secondary voltage, Frequency, Cooling type, Temperature rise, Time stamp, Load current, Temperature, Power factor, Active power, Reactive power");
+                    foreach (var log in transobj.component.loggedData)
+                        { sb.Append($"{transobj.LogStart:yyyy-MM-dd HH:mm:ss},{transobj.LogEnd:yyyy-MM-dd HH:mm:ss}," +
+                                    $"{transobj.component.spec?.component_id}, {transobj.component.spec?.component_type}, {transobj.component.spec?.power_rating}, {transobj.component.spec?.primary_voltage}, " +
+                                    $"{transobj.component.spec?.secondary_voltage}, {transobj.component.spec?.frequency}, {transobj.component.spec?.cooling_type}" +
+                                    $"{transobj.component.spec?.temperature_rise}, {log.timestamp:yyyy-MM-dd HH:mm:ss}, {log.load_current}, {log.temperature}" +
+                                    $"{log.power_factor}, {log.active_power}, {log.reactive_power}"  ); 
+                    }
+
                 }
+                if (logObject is PowerlineLogObject)
+                {
+                        PowerlineLogObject powlobj = logObject as PowerlineLogObject;
+                        if (powlobj.component?.loggedData == null)
+                            continue; // Skip if there's no log data
+                    sb.AppendLine( "Log start, Log end, Component Id, Component type, Length (km), current capacity, max operating temperature, conductor material, line type, insulation type, Time stamp, power capacity, line voltage");
+                   foreach (var log in powlobj.component.loggedData)
+                    {
+                        sb.AppendLine($"{powlobj.LogStart:yyyy-MM-dd HH:mm:ss},{powlobj.LogEnd:yyyy-MM-dd HH:mm:ss}," +
+                                      $"{powlobj.component.spec?.component_id}, {powlobj.component.spec?.component_type}, {powlobj.component.spec?.length_km},{powlobj.component.spec?.current_capacity},{powlobj.component.spec?.max_operating_temperature}," +
+                                      $"{powlobj.component.spec?.conductor_material},{powlobj.component.spec?.line_type},{powlobj.component.spec?.insulation_type}," +
+                                      $"{log.timestamp:yyyy-MM-dd HH:mm:ss},{log.power_capacity},{log.line_voltage}");
+                    }
+                }
+                string csvfilename = $"logdata{logCount++}.csv";
+                string fullFilePath = filePath + csvfilename;
+                File.WriteAllText(fullFilePath, sb.ToString());
             }
 
             // Write to CSV file
-            File.WriteAllText(filePath, sb.ToString());
 
             Console.WriteLine("CSV file successfully written!");
         }
